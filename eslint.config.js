a1f302fcd982462209ec5877fca5dfcc85132a80
Kai Nakazawa <k.n.bluestar@gmail.com>
2024-05-12 18:21:13 +0900

    dev: Set up testing and linting commands.
    - Set up jest and test-watch script for continuous testing
    - Set up webpack to work in watch mode with ES modules

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,11 @@ import js from '@eslint/js';
 
 export default [
   js.configs.recommended,
-
   {
+    env: {
+      jest: true,
+      browser: true,
+    },
     rules: {
       'no-unused-vars': 'warn',
       'no-undef': 'warn',
